af163fe40ae63712dbf54769fa413dbc51d2b92a
juuliag <juhgasparello@gmail.com>
2026-05-07 13:43:21 -0300

    Add files via upload

--- PW1/Currículo/curriculo/JS/script.js
+++ PW1/Currículo/curriculo/JS/script.js
@@ -3,6 +3,9 @@ const botao = document.getElementById("Tema");
 // carregar preferência
 if (localStorage.getItem("tema") === "dark") {
     document.body.classList.add("dark");
+    botao.textContent = "☀️​"; // sol quando já começa o modo escuro
+}else{
+    botao.textContent = "🌗​​"; // lua no modo claro
 }
 
 // alternar tema
@@ -11,7 +14,9 @@ botao.addEventListener("click", () => {
 
     if (document.body.classList.contains("dark")) {
         localStorage.setItem("tema", "dark");
+        botao.textContent = "☀️​"; // muda para sol
     } else {
         localStorage.setItem("tema", "light");
+        botao.textContent = "🌗​​"; // muda para lua
     }
 });
